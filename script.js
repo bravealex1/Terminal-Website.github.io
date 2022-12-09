dd2775d214caf54b3b7f8cdbffcd049a547b70b6
Alex <20010426alex@gmail.com>
2022-12-08 21:21:52 -0500

    Food Website Commit

--- script.js
+++ script.js
@@ -17,9 +17,18 @@ function execute(input){
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
     terminal_content.scrollTop = terminal_content.scrollHeight;
 
-    if(input == "github"){
-        setTimeout(() => {window.open("https://github.com/chenkuanliao", "_blank");}, 2000);
+    if(input == "food"){
+        setTimeout(() => {window.open("https://www.hellofresh.com/pages/meal-kit-delivery?c=MD-6EZ7SWBQ2&dm=meals&dm_gifts=gifts&mealsize=4-2&utm_campaign=SEA_Bing_Brand_Refresh_2022_18FM&utm_source=bing&utm_medium=cpc&utm_content=act_seabrand_seabrand&utm_id=2f850055-a3b7-45a5-b3f8-6f4fdb3aab6c&utm_source=bing&utm_medium=cpc&utm_campaign=US-S%5B01%5D%3A%20Prospecting%20%7C%20Brand%20%7C%20Quality%20-%20Broad&utm_content=Frozen&utm_term=hello%20fresh%20gourmet&utm_id=bi~377410507~1243548166308209~kwd-77722084004709:loc-190~bb&msclkid=39cbb2e1a0a712459a1fd173c9ec6b8b", "_blank");}, 2000);
     }
+
+    if(input == "snacks"){
+        setTimeout(() => {window.open("https://www.gopuff.com/", "_blank");}, 2000);
+    }
+
+    if(input == "drinks"){
+        setTimeout(() => {window.open("https://beverageuniverse.com/", "_blank");}, 2000);
+    }
+
 }
 
 function key(e){
@@ -39,18 +48,18 @@ document.addEventListener("keypress", key);
 const COMMANDS = 
 {
     help:
-        'Supported commands: <span class="command-keyword">"about"</span>, <span class="command-keyword">"experience"</span>, <span class="command-keyword">"education"</span>, <span class="command-keyword">"skills"</span>, <span class="command-keyword">"contact"</span>, <span class="command-keyword">"github"</span>',
-    about:
-        "Hello, my name is Brian, not Brain. I like chocolate and I don't like carrots",
-    experience:
-        'I wrote "hello world" in python, and I felt that I have the power in my hand!',
-    education:
-        'I learned to eat by myself at the age of 3. Impressive, isn\'t it?',
-    skills:
-        'I can hold three eggs in my mouth at the same time. Can you?',
-    contact:
-        'You can never find me! HAHAHA!',
-    github:
-        'opening my GitHub page ...'
+        'Supported commands: <span class="command-keyword">"food"</span>, <span class="command-keyword">"snacks"</span>, <span class="command-keyword">"drinks"</span>',
+    food:
+        "Hello, this is our food meun, please select from the food option listed to add to your order",
+    snacks:
+        'Hello, this is our snacks meun, please select from the food option listed to add to your order',
+    drinks:
+        'Hello, this is our snacks meun, please select from the food option listed to add to your order',
+    // skills:
+    //     'I can hold three eggs in my mouth at the same time. Can you?',
+    // contact:
+    //     'You can never find me! HAHAHA!',
+    // github:
+    //     'opening my GitHub page ...'
     
 }

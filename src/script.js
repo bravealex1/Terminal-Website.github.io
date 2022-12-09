3703dcdc4cff14ed201f91d8f0936391f049530e
Alex <20010426alex@gmail.com>
2022-12-09 15:19:30 -0500

    change background image

--- src/script.js
+++ src/script.js
@@ -17,18 +17,17 @@ function execute(input){
     terminal_outputs.innerHTML = `${terminal_outputs.innerHTML+output}`;
     terminal_content.scrollTop = terminal_content.scrollHeight;
 
-    if(input == "food"){
-        setTimeout(() => {window.open("https://www.hellofresh.com/pages/meal-kit-delivery?c=MD-6EZ7SWBQ2&dm=meals&dm_gifts=gifts&mealsize=4-2&utm_campaign=SEA_Bing_Brand_Refresh_2022_18FM&utm_source=bing&utm_medium=cpc&utm_content=act_seabrand_seabrand&utm_id=2f850055-a3b7-45a5-b3f8-6f4fdb3aab6c&utm_source=bing&utm_medium=cpc&utm_campaign=US-S%5B01%5D%3A%20Prospecting%20%7C%20Brand%20%7C%20Quality%20-%20Broad&utm_content=Frozen&utm_term=hello%20fresh%20gourmet&utm_id=bi~377410507~1243548166308209~kwd-77722084004709:loc-190~bb&msclkid=39cbb2e1a0a712459a1fd173c9ec6b8b", "_blank");}, 2000);
+    if(input == "github"){
+        setTimeout(() => {window.open("https://github.com/bravealex1", "_blank");}, 2000);
     }
-
-    if(input == "snacks"){
-        setTimeout(() => {window.open("https://www.gopuff.com/", "_blank");}, 2000);
+    
+     if(input == "resume"){
+        setTimeout(() => {window.open("https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:a85331b8-29e8-36e4-aa64-1ffc0daf2209", "_blank");}, 2000);
     }
-
-    if(input == "drinks"){
-        setTimeout(() => {window.open("https://beverageuniverse.com/", "_blank");}, 2000);
+    
+     if(input == "linkedin"){
+        setTimeout(() => {window.open("https://www.linkedin.com/in/alex-zhu802/", "_blank");}, 2000);
     }
-
 }
 
 function key(e){
@@ -45,21 +44,25 @@ function key(e){
 
 document.addEventListener("keypress", key);
 
-const COMMANDS = 
+const COMMANDS =
 {
     help:
-        'Supported commands: <span class="command-keyword">"food"</span>, <span class="command-keyword">"snacks"</span>, <span class="command-keyword">"drinks"</span>',
-    food:
-        "Hello, this is our food meun, please select from the food option listed to add to your order",
-    snacks:
-        'Hello, this is our snacks meun, please select from the food option listed to add to your order',
-    drinks:
-        'Hello, this is our snacks meun, please select from the food option listed to add to your order',
-    // skills:
-    //     'I can hold three eggs in my mouth at the same time. Can you?',
-    // contact:
-    //     'You can never find me! HAHAHA!',
-    // github:
-    //     'opening my GitHub page ...'
-    
+    'Supported commands: <span class="command-keyword">"about"</span>, <span class="command-keyword">"experience"</span>, <span class="command-keyword">"education"</span>, <span class="command-keyword">"softskills"</span>, <span class="command-keyword">"contact"</span>, <span class="command-keyword">"github"</span> <span class="command-keyword">"resume"</span> <span class="command-keyword">"resume"</span>',
+        about:
+        "Hello, my name is Alex, and I am a fourth-year student (senior) at Penn State majoring in Information Science and Technology with a focus on design and development. I am a hard-working and self-driving student who strives to succeed and achieve my career and personal goals. I have enhanced my java programming skills in the past two years, completing multiple projects in and outside classes. Since last semester, I have been self-learning python and R programming languages in Dataquest and Datacamp. In addition, I am involved in Nittany AI Challenge and teaming up with others to figure out solutions to increase crop yield by predicting pathogens and reducing economic losses for farmers. My future goal is to apply what I learned in college to the whole world and help to build a better world!",
+        experience:
+        "I am the vice president of the Nittany Data Lab, and I am the front-end developer",
+        education:
+        "I am an Honors student at Pennsylvania State University",
+        skills:
+        "Java, HTML, CSS, SQL, Node.js, Express.js, JavaScript, Machine Learning, Python",
+        contact:
+        "email: 20010426alex@gmail.com, phone number: 407-619-6572",
+        github:
+        "https:opening my github",
+        resume:
+        'opening my resume',
+        linkedin:
+        'opening my linkedin'
+
 }
